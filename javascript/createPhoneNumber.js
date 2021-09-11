3248c39af9badeed3813fca722e683a0a7f1db3a
matheusg18 <matheusgabriel.18@gmail.com>
2021-09-11 16:15:53 -0300

    createPhoneNumber feito

---
+++ javascript/createPhoneNumber.js
@@ -0,0 +1,8 @@
+// https://www.codewars.com/kata/525f50e3b73515a6db000b83
+
+const createPhoneNumber = (numbers = []) => `(${numbers.slice(0, 3).join('')}) ${numbers.slice(3, 6).join('')}-${numbers.slice(6).join('')}`;
+
+const Test = require('assert');
+Test.strictEqual(createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]), '(123) 456-7890');
+Test.strictEqual(createPhoneNumber([1, 1, 1, 1, 1, 1, 1, 1, 1, 1]), '(111) 111-1111');
+Test.strictEqual(createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]), '(123) 456-7890');
